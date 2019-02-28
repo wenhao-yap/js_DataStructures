4df1e3b94b82798e7b1221337276193234298877
wenhao-yap <jack4spadesz@gmail.com>
2019-02-28 14:25:04 +0800

    added 28-Implement strStr()

--- Easy/28_ImplementstrSr().js
+++ Easy/28_ImplementstrSr().js
@@ -11,13 +11,23 @@ Output: 2
 Example 2:
 Input: haystack = "aaaaa", needle = "bba"
 Output: -1
+
+Clarification:
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 --------------------------------------------------------------------------------------*/
 
 /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
+ * runtime: 56 ms
  */
 var strStr = function(haystack, needle) {
-    
+  if(!needle.length) return 0;
+  return haystack.indexOf(needle);
 };
+
+console.log(strStr("hello",""));
+console.log(strStr("hello","ll"));
+console.log(strStr("aaaaa","bba"));
